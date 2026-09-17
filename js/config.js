e06b1d6e67fa59b59d086f9ab76fe95aeed72fdb
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-17 16:59:13 -0300

    Separa configuración editable de la lógica

---
+++ js/config.js
@@ -0,0 +1,24 @@
+window.INVITATION_CONFIG = {
+  invitation: {
+    name: "Samuel",
+    age: 7,
+    message: "¡Prepárate para una aventura increíble en el Reino Champiñón! ¿Listo para saltar, correr y celebrar mi cumple? ¡Te espero!",
+    eventDateTime: "2026-10-03T18:00:00-03:00",
+    eventEndDateTime: "2026-10-03T21:00:00-03:00",
+    dateLabel: "Sábado 3 de Octubre",
+    timeLabel: "18:00 a 21:00 hs.",
+    venue: "Salón de Fiestas Janos",
+    address: "Gral. Pedro Díaz 1800, Hurlingham",
+    mapUrl: "https://maps.app.goo.gl/LnZVf7kF9egGp7Lk8",
+    dressCode: "Si te copás, podés venir al cumple disfrazado 😁",
+    whatsappNumber: "5491156223007",
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Samuel. 🎮🍄",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/"
+  },
+  characters: [
+    { src: "assets/img/2.png", width: "min(88vw, 450px)", bottom: "-2px", translateX: "-50%" },
+    { src: "assets/img/1.png", width: "min(86vw, 440px)", bottom: "-4px", translateX: "-51%" },
+    { src: "assets/img/3.png", width: "min(103vw, 530px)", bottom: "-4px", translateX: "-50%" },
+    { src: "assets/img/4.png", width: "min(101vw, 520px)", bottom: "-7px", translateX: "-50%" }
+  ]
+};
