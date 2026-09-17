a657e3993bb305da7e29059da7bd1d77a7d7344e
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-17 18:44:07 -0300

    Alinea configuración con el diseño original mejorado

--- js/config.js
+++ js/config.js
@@ -10,15 +10,16 @@ window.INVITATION_CONFIG = {
     venue: "Salón de Fiestas Janos",
     address: "Gral. Pedro Díaz 1800, Hurlingham",
     mapUrl: "https://maps.app.goo.gl/LnZVf7kF9egGp7Lk8",
-    dressCode: "Si te copás, podés venir al cumple disfrazado 😁",
+    extraMessage: "Si te copás, podés venir al cumple disfrazado 😁",
     whatsappNumber: "5491156223007",
-    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Samuel. 🎮🍄",
-    instagramUrl: "https://www.instagram.com/rossdigitalstudio/"
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Samuel. 🍄⭐",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
+    instagramHandle: "@rossdigitalstudio"
   },
   characters: [
-    { src: "assets/img/2.png", width: "min(88vw, 450px)", bottom: "-2px", translateX: "-50%" },
-    { src: "assets/img/1.png", width: "min(86vw, 440px)", bottom: "-4px", translateX: "-51%" },
-    { src: "assets/img/3.png", width: "min(103vw, 530px)", bottom: "-4px", translateX: "-50%" },
-    { src: "assets/img/4.png", width: "min(101vw, 520px)", bottom: "-7px", translateX: "-50%" }
+    { src: "assets/img/2.png", width: "min(86vw, 395px)", bottom: "-3px", translateX: "-50%" },
+    { src: "assets/img/1.png", width: "min(88vw, 405px)", bottom: "-4px", translateX: "-50%" },
+    { src: "assets/img/3.png", width: "min(101vw, 470px)", bottom: "-4px", translateX: "-50%" },
+    { src: "assets/img/4.png", width: "min(100vw, 465px)", bottom: "-5px", translateX: "-50%" }
   ]
 };
